c68933d8c68291555ad471ac52ebe10b13df1dc0
JackFuckinTwist <juanma.anse@hotmail.com>
2017-09-09 18:21:44 -0300

    First web upload

---
+++ SCRIPT/script.js
@@ -0,0 +1,29 @@
+    var logo = document.getElementById("logo") ;
+    var container = document.getElementById("container-nav-respons-id");
+var iconos = document.getElementById("iconos-redes");
+
+function dropdown() {
+    
+    if (container.className === "container-nav-respons") {
+        container.className += " container-nav-responsdown";
+        logo.style.opacity = "0"; 
+    iconos.style.marginLeft = "20.0342vw";
+    }
+    
+    else {
+        container.className = "container-nav-respons";
+        logo.style.opacity = "1";
+        iconos.style.marginLeft = "-12vw";
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
